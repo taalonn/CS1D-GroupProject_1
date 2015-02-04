aa4ffcecc68d12366e922a2f8c8db8166a27d5b9
Xannada <Xannada@users.noreply.github.com>
2015-02-04 00:44:08 -0800

    a little start
    just did a little ground work, feeling like i need to start something

---
+++ build-CS1D-Project1_QT-Desktop_Qt_5_3_MinGW_32bit-Debug/ui_mainwindow.h
@@ -0,0 +1,115 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.3.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionCommon_Help_Topics;
+    QAction *actionContact_Us;
+    QWidget *centralWidget;
+    QLabel *label;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QMenuBar *menuBar;
+    QMenu *menuHelp;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->setEnabled(true);
+        MainWindow->resize(848, 582);
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        MainWindow->setMinimumSize(QSize(848, 582));
+        MainWindow->setMaximumSize(QSize(848, 582));
+        MainWindow->setAutoFillBackground(false);
+        actionCommon_Help_Topics = new QAction(MainWindow);
+        actionCommon_Help_Topics->setObjectName(QStringLiteral("actionCommon_Help_Topics"));
+        actionContact_Us = new QAction(MainWindow);
+        actionContact_Us->setObjectName(QStringLiteral("actionContact_Us"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(0, -10, 851, 571));
+        label->setPixmap(QPixmap(QString::fromUtf8(":/pics/J:/CS1D/WineryProject/vineyard pic 1.jpg")));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(10, 520, 211, 21));
+        pushButton->setAutoFillBackground(false);
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(330, 520, 211, 21));
+        pushButton_3 = new QPushButton(centralWidget);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+        pushButton_3->setGeometry(QRect(630, 520, 211, 21));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 848, 21));
+        menuHelp = new QMenu(menuBar);
+        menuHelp->setObjectName(QStringLiteral("menuHelp"));
+        MainWindow->setMenuBar(menuBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuHelp->menuAction());
+        menuHelp->addAction(actionCommon_Help_Topics);
+        menuHelp->addAction(actionContact_Us);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Welcome", 0));
+        actionCommon_Help_Topics->setText(QApplication::translate("MainWindow", "Common Help Topics", 0));
+        actionContact_Us->setText(QApplication::translate("MainWindow", "Contact Us", 0));
+        label->setText(QString());
+        pushButton->setText(QApplication::translate("MainWindow", "PushButton", 0));
+        pushButton_2->setText(QApplication::translate("MainWindow", "PushButton", 0));
+        pushButton_3->setText(QApplication::translate("MainWindow", "PushButton", 0));
+        menuHelp->setTitle(QApplication::translate("MainWindow", "Help", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
