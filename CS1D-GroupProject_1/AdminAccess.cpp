233de5a932cbd89037f7f04604db3e3444be9107
kyleguss <couchpotatokylecouchpotatokyle@me.com.com>
2015-02-10 16:52:04 -0800

    Admin Access Boolean Function
    Thy function taketh two strings, to determine if thy user shallst be
    granted admin access.
    Seriously, pass it a username and password and it will compare them to
    the hard coded admin user and password.

---
+++ CS1D-GroupProject_1/AdminAccess.cpp
@@ -0,0 +1,20 @@
+bool AdminAccess(string attemptUser, string attemptPass)
+{
+	bool gainAccess;
+	gainAccess = false;
+
+	string realUser;
+	string realPass;
+
+	realUser = "schoolgirl";
+	realPass = "talonlikesboys";
+
+	if ((attemptUser == realUser) && (attemptPass == realPass))
+	{
+		cout << "Admin Access Approved." << endl;
+		gainAccess = true;
+	}
+
+	return gainAccess;
+}
+
