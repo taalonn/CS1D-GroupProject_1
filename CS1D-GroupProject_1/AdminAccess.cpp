4f52e3e45dd7d8eb2f048f7612238d5e3902001a
Talon Baker <taalonn@gmail.com>
2015-02-12 20:57:48 -0800

    talon likes men too

--- CS1D-GroupProject_1/AdminAccess.cpp
+++ CS1D-GroupProject_1/AdminAccess.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+//Admin Access function will take in a user account name and password and
+//check them against the database of accounts and passwords already stored.
+//returns true if valid login
 bool AdminAccess(string attemptUser, string attemptPass)
 {
 	bool gainAccess;
@@ -19,5 +22,5 @@ bool AdminAccess(string attemptUser, string attemptPass)
 	}
 
 	return gainAccess;
-}
+}//end - AdminAccessS
 
