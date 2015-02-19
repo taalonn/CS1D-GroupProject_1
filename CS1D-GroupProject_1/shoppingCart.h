9d2e63c11605c120e08a60085d7b59013d302cbd
Seth-Taylor <seth.michael.taylor@gmail.com>
2015-02-18 20:51:25 -0800

    Create shoppingCart.h

---
+++ CS1D-GroupProject_1/shoppingCart.h
@@ -0,0 +1,57 @@
+#ifndef SHOPPINGCART_H
+#define	SHOPPINGCART_H
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+class shoppingCart
+{
+private:
+    float  total;
+    string out;
+    
+public:
+    shoppingCart();
+    ~shoppngCart();
+    void Add(float purchase);
+    void UpdateOutput(string wine, float purchase, int amount);
+    string GetOutput();   
+};
+
+shoppingCart::shoppingCart()
+{
+    total  = 0;
+    out    = " #   Purchase   Cost \n"
+             "--- ---------- ------\n";
+}
+
+shoppingCart::~shoppngCart()
+{}
+
+void shoppingCart::Add(float purchase)
+{
+    total += purchase;
+}
+
+void shoppingCart::UpdateOutput(string wine, float purchase, int amount)
+{
+    ostringstream update = out;
+    
+    update << left << setw(3) << amount << "\s\s" << setw(9) << wine 
+           << "\s\s" << setw(6) << purchase*amount << endl;
+    Add(purchase*amount);
+    out = update;
+}
+
+string shoppingCart::GetOutput()
+{
+     ostringstream update = out;
+     update << "---------------------\n"
+            << "Total: $ " << total;
+     out = update;
+     return out;
+}
+#endif	/* SHOPPINGCART_H */
+
